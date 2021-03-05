7ecab706b76c7f28cbcc37568f4484d37a99fe1a
mining21 <anandasyahputra96@gmail.com>
2021-03-05 17:53:50 +0700

    project saya

---
+++ ethminer19.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x136c85ede98a3f16e8789b4039244edb695dcc85.weda@eu1.ethermine.org:4444
